f48b5e8060debf1126d617ac9982e2a7cb60af7e
shiva <gbhvaniprasad1818@gmail.com>
2026-05-31 20:51:04 +0530

    roboshop automation

--- without-common.sh/roboshop-common.sh
+++ without-common.sh/roboshop-common.sh
@@ -31,9 +31,14 @@ if [ "$ACTION" != "create" ] && [ "$ACTION" != "delete" ]; then
     exit 1
 fi
 
-get_instance_id(){
+get_instance_id() {
     name=$1
-    aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-common-$name" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text
+
+    aws ec2 describe-instances \
+        --filters "Name=tag:Name,Values=roboshop-common-${name}" \
+                  "Name=instance-state-name,Values=running" \
+        --query "Reservations[0].Instances[0].InstanceId" \
+        --output text
 }
 
 for instance in $@
